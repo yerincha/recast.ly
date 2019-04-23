ac0ddf8f0de00be045162b34cce3b371f9883f13
lenacha <lenacha32@gmail.com>
2019-04-23 12:32:35 -0700

    Implementing video player title is clicked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,48 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentVideo: exampleVideoData[0], 
+      videoList: exampleVideoData
+    };
+    this.titleClick.bind(this);
+  }
+
+  titleClick() {
+    this.setState ({
+      currentVideo: arguments[0],
+      videoList: exampleVideoData
+    });
+  }
+
+  
+
+  render() {
+    return (
+      <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em><Search /></h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><VideoPlayer video={exampleVideoData[0]}/></div>
+        </div>
+        <div className="col-md-5">
+          <div><VideoList titleClick={this.titleClick} videos={this.state.videoList}/></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+    )
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
