38344247bda331499af226a9f594f82bedb90db3
lenacha <lenacha32@gmail.com>
2019-04-22 20:53:30 -0700

    Implement rendering skeleton to DOM.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,8 @@
-
 import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <VideoListEntry video={props} />
   </div>
 );
 
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,16 +1,20 @@
-var VideoListEntry = (props) => (
-  <div className="video-list-entry media">
-    <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
-    </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">
-        {props.snippet.title}
+class VideoListEntry extends React.Component {
+  render () {
+    return (
+      <div className="video-list-entry media">
+        <div className="media-left media-middle">
+          <img className="media-object" src={exampleVideoData.snippet.thumbnails.default.url} alt="" />
+        </div>
+        <div className="media-body">
+          <div className="video-list-entry-title">{exampleVideoData.snippet.title}
+          </div>
+          <div className="video-list-entry-detail">{exampleVideoData.snippet.description}
+          </div>
+        </div>
       </div>
-      <div className="video-list-entry-detail">{props.snippet.description}</div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
